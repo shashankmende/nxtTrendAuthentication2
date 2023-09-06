fcc29be88e95b998064e6237293b7759d90de356
shashank <shashankmende88@gmail.com>
2023-09-06 09:19:18 +0000

    main

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+    />
+    <h1>Cart</h1>
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Products = () => (
+  <div className="products-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+      alt="products"
+    />
+    <h1>Products</h1>
+  </div>
+)
+
+export default Products
